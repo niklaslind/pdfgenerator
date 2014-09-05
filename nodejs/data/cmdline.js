adbac32ae6a8b2822e4ed7e4fb4f4a6d57a8a1ba
Niklas Lind <nlin@kth.se>
2014-09-05 12:53:37 +0200

    refactor: delete mvn and grails, keep nodejs

--- nodejs/data/cmdline.js
+++
@@ -1,7 +0,0 @@
-var fs = require('fs');
-var pdf = require('html-pdf');
-var html = fs.readFileSync('./data/elastx.html', 'utf8')
-pdf.create(html, {"format": "A4" }, function(err, buffer) {
-  if (err) return console.log(err);
-  fs.writeFile('cmd.pdf', buffer);
-});
